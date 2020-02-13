6a224b0deb07e4adf76d05f1fbb6e1abb1f944c2
npagan <npagan@proforma.com>
2020-02-12 22:55:57 -0500

    Partially Finished Adventure Mode!

--- Program.cs
+++ Program.cs
@@ -8,11 +8,16 @@ namespace DeckOCards
     static void Main(string[] args)
     {
 
+      Console.Clear();
+
       Console.WriteLine("Welcome to my Deck of Cards! Let me pick a card for you...");
+      Console.WriteLine();
+
       var suit = new string[] { "Clubs", "Diamonds", "Hearts", "Spades" };
       var value = new string[] { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
       var card = "";
       var deck = new List<string>();
+      var playerHand = new List<string>();
 
       for (var i = 0; i < suit.Length; i++)
       {
@@ -27,15 +32,16 @@ namespace DeckOCards
       for (var i = deck.Count - 1; i >= 1; i--)
       {
         var j = new Random().Next(i);
-
         var temp = (deck[j]);
         deck[j] = (deck[i]);
         deck[i] = (temp);
       }
 
       Console.WriteLine($"Your card is the {deck[0]}");
-      deck.RemoveAt(0);
+      Console.WriteLine();
 
+      playerHand.Add(deck[0]);
+      deck.RemoveAt(0);
 
 
       bool hitMe = false;
@@ -43,22 +49,33 @@ namespace DeckOCards
       {
         Console.WriteLine("Would you like to draw the next card or quit?");
         Console.WriteLine("Please select: Next or Quit...");
+        Console.WriteLine();
+
         var userInput = Console.ReadLine().ToLower();
         if (userInput == "next")
         {
           Console.WriteLine($"Your next card is {deck[0]}");
+          Console.WriteLine();
+
+          playerHand.Add(deck[0]);
           deck.RemoveAt(0);
+
+
+          Console.WriteLine($"Your hand now consists of ...");
+          Console.WriteLine();
+
+          for (var i = 0; i < playerHand.Count; i++)
+          {
+            Console.WriteLine(playerHand[i]);
+          }
+
+          Console.WriteLine();
         }
         else if (userInput == "quit")
         {
           hitMe = true;
         }
       }
-
-
-
-
-
     }
   }
 }
